8b77908aee228ca34dcfa99dbffc3ec0d1c588fb
Aditya Pareek <pareekaditya01@gmail.com>
2023-02-09 17:26:39 +0530

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:e_cart/pages/Home_Page.dart';
 import 'package:e_cart/pages/login_page.dart';
 import 'package:e_cart/utils/routes.dart';
+import 'package:e_cart/widgets/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
+      theme: Mytheme.lightTheme(context),
+      darkTheme: Mytheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../widgets/drawer.dart';
 
@@ -9,9 +10,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
         centerTitle: true,
-        title: Text("E-Cart"),
+        title: Text("E-Cart",),
       ),
       body: Center(
         child: Container(
